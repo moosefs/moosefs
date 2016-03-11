f426a2f80379638644a98212f169766a150831b1
Jakub Kruszona-Zawadzki <acid@moosefs.com>
2016-03-11 13:28:05 +0100

    added missing file 'mfsalloc.h'

---
+++ mfscommon/mfsalloc.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2016 Jakub Kruszona-Zawadzki, Core Technology Sp. z o.o.
+ * 
+ * This file is part of MooseFS.
+ * 
+ * MooseFS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 (only).
+ * 
+ * MooseFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MooseFS; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA
+ * or visit http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+#ifndef _MFSALLOC_H_
+#define _MFSALLOC_H_
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdlib.h>
+
+#ifdef HAVE_REALLOCF
+#define mfsrealloc reallocf
+#else
+static inline void* mfsrealloc(void *ptr,size_t size) {
+	void *pptr = realloc(ptr,size);
+	if (pptr==NULL) {
+		free(ptr);
+	}
+	return pptr;
+}
+#endif
+
+#endif

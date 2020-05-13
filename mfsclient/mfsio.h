a4f89aae9fae993abebe25e5c9653c14464fa8c5
Jakub Kruszona-Zawadzki <acid@moosefs.com>
2020-05-13 10:24:00 +0200

    (mfsio/nbd) fixed compile issue on systems without LOCK_* constants (issue #366)

--- mfsclient/mfsio.h
+++ mfsclient/mfsio.h
@@ -29,6 +29,19 @@ typedef struct _mfscfg {
 # define UTIME_OMIT	((1l << 30) - 2l)
 #endif
 
+#ifndef LOCK_SH
+#define LOCK_SH 1
+#endif
+#ifndef LOCK_EX
+#define LOCK_EX 2
+#endif
+#ifndef LOCK_NB
+#define LOCK_NB 4
+#endif
+#ifndef LOCK_UN
+#define LOCK_UN 8
+#endif
+
 int mfs_mknod(const char *path, mode_t mode, dev_t dev);
 int mfs_unlink(const char *path);
 int mfs_mkdir(const char *path, mode_t mode);

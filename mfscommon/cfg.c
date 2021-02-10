5cecb9c2409746c7306f7757262d0599ca6c4c0c
Jakub Kruszona-Zawadzki <acid@moosefs.com>
2021-02-10 13:08:57 +0100

    (all) allow full 8bit chars in option values (for future use)

--- mfscommon/cfg.c
+++ mfscommon/cfg.c
@@ -98,7 +98,7 @@ int cfg_reload (void) {
 		i++;
 		while (linebuff[i]==' ' || linebuff[i]=='\t') i++;
 		vps = i;
-		while (linebuff[i]>=32 && linebuff[i]<127) {
+		while (linebuff[i]>=32) {
 			i++;
 		}
 		while (i>vps && linebuff[i-1]==32) {

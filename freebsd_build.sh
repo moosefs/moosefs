82888573e6af2e1be4820203f879a923cd366860
Jakub Kruszona-Zawadzki <acid@moosefs.com>
2021-07-05 14:10:29 +0200

    (freebsd) added default user and group to freebsd building script

--- freebsd_build.sh
+++ freebsd_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./configure --localstatedir=/var --with-mfscgiserv-dir=/usr/local/sbin --with-mfscgi-dir=/usr/local/share/mfscgi
+./configure --localstatedir=/var --with-mfscgiserv-dir=/usr/local/sbin --with-mfscgi-dir=/usr/local/share/mfscgi --with-default-user=mfs --with-default-group=mfs
 make
